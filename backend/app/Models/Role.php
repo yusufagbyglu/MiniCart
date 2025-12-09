64b07d56cbee368179bbd92bfac92d3ea2e68be6
yusufagbyglu <myusufagabeyoglu@gmail.com>
2025-12-09 17:27:05 +0300

    feat add user,permission,role migrations

---
+++ backend/app/Models/Role.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    public function user(){
+        return $this->belongsToMany(User::class, 'user_roles')->withTimestamps();
+
+    }
+}
--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -46,4 +46,19 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function roles(){
+        return $this->belongsToMany(Role::class, 'user_roles')->withTimestamps();
+    }
+
+    public function hasRole($roleName){
+        return $this->roles()->where('name', $roleName)->exists();
+    }
+
+    public function hasPermission($permissionName){
+
+        return $this->roles()->whereHas('permissions', function($q) use ($permissionName) {
+            $q->where('name', $permissionName);
+        })->exists();
+    }
 }
