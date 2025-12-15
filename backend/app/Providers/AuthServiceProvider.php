4d77de673053b8016e989e4b0541431dd07a7390
yusufagbyglu <myusufagabeyoglu@gmail.com>
2025-12-15 17:27:06 +0300

    feat: add AuthServiceProvider.php

---
+++ backend/app/Providers/AuthServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use App\Models\User;
+use App\Policies\UserPolicy;
+use App\Models\Role;
+use App\Models\Product;
+use App\Policies\ProductPolicy;
+
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        User::class => UserPolicy::class,
+        Product::class => ProductPolicy::class,
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     */
+    public function boot(): void
+    {
+        $this->registerPolicies();
+
+        // Buraya Gate tanımları ekleyebilirsiniz
+    }
+}
