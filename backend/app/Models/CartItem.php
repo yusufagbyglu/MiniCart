b3980cc32001f5246b525bf10682be3a48601b58
yusufagbyglu <myusufagabeyoglu@gmail.com>
2025-12-28 22:39:42 +0300

    feat: add relations to CartItem model

--- backend/app/Models/CartItem.php
+++ backend/app/Models/CartItem.php
@@ -3,8 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CartItem extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected $fillable = ['cart_id', 'product_id', 'quantity'];
+
+    /**
+     * Get the cart that owns the cart item.
+     */
+    public function cart()
+    {
+        return $this->belongsTo(Cart::class);
+    }
+
+    /**
+     * Get the product that the cart item belongs to.
+     */
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
